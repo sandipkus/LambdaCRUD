b7ce24338b7b7565377a3d743c7194c59d901085
unknown <sandipkus1@gmail.com>
2022-12-22 11:28:50 +0530

    first push

---
+++ index.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+var connection = mysql.createPool({
+    host: 'database-1.csbyxhiw6onu.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    port: '3306',
+    password: 'Password',
+    database: 'my_db'
+});
+
+exports.handler = (event, context, callback) =>{
+    console.log("request:",JSON.stringify(event));
+    let body = {};
+    if(event.body){
+     body = JSON.parse(event.body);
+    }
+      context.callbackWaitsForEmptyEventLoop = false;
+        connection.query(`INSERT INTO student (id,name,email,password) VALUES (${body.id},'${body.name}','${body.email}','${body.password}')`,(err,res) => {
+        if(err) throw err;
+        let result = {
+            statusCode : 200,
+            body:JSON.stringify(res)
+        };
+        callback(null,result);
+        
+    });
+};
