41f24ffddbc8486712ab7fc08e201070224337d5
unknown <sandipkus1@gmail.com>
2022-12-22 11:34:38 +0530

    pushed delete code

---
+++ index.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+var connection = mysql.createPool({
+    host: 'database-1.csbyxhiw6onu.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    port: '3306',
+    password: 'Password',
+    database: 'my_db'
+});
+
+exports.handler = (event, context, callback) =>{
+    console.log("request:" +JSON.stringify(event));
+  
+    context.callbackWaitsForEmptyEventLoop = false;
+      let id=event.queryStringParameters.id;
+      let sql ="DELETE FROM student WHERE id=?";
+
+    connection.query(sql,[id],(err,res)=>{
+        if(err){
+            throw err
+        }
+        let result = {
+            statusCode : 200,
+            body: JSON.stringify(res)
+        }
+
+        callback(null,result)
+    })
+}
